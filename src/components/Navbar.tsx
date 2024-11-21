2d11a7223f23c87040a771c7068200bc8dd367e1
PARMESH YADAV <parmesh20319@iiitd.ac.in>
2024-11-21 21:13:12 +0530

    added media queries in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,11 @@ import MaxWidthWrapper from "./MaxWidthWrapper"
 import { useState } from "react"
 import { cn } from "@/lib/utils"
 import { Input } from "./ui/input"
-import { Search } from "lucide-react"
+import { Search, Menu, Youtube } from "lucide-react"
 import { usePathname, useRouter } from "next/navigation"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
-const LANGUAGES = ["English", "हिंदी", "اردو"]
+const LANGUAGES = ["English", "हिंदी", "اردू"]
 const MENUITEMS = [
     { name: "Home", path: "/" },
     { name: "Politics", path: "/categories/politics/all" },
@@ -25,14 +26,13 @@ const OTHERITEMS = [
     { name: "Opinions", path: "/opinions" },
 ]
 
-
 const Navbar = () => {
-
     const router = useRouter();
     const pathName = usePathname();
 
     const [selectedLanguage, setSelectedlanguage] = useState(LANGUAGES[0]);
     const [searchQuery, setSearchQuery] = useState('');
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchQuery(e.target.value);
@@ -44,29 +44,34 @@ const Navbar = () => {
         }
     }
 
+    const handleYoutubeClick = () => {
+        window.open("https://www.youtube.com/channel/UCt0n9skCrGvC97AIywt6o7A", "_blank", "noopener,noreferrer");
+    }
+
     return (
         <nav className="flex flex-col">
-            {/* White Bar */}
+            {/* White Bar - Large Screens */}
             <div className="bg-white">
                 <MaxWidthWrapper>
                     <div className="flex items-center justify-between">
-                        {/* left div */}
-                        <div className="flex items-center gap-x-4">
-                            {/* Logo */}
-                            <div>
-                                <Image
-                                    src='/img/logo.png'
-                                    alt='Logo'
-                                    width={100}
-                                    height={100}
-                                    onClick={() => router.push('/')}
-                                />
-                            </div>
-                            {/* Language Options */}
-                            <div>
-                                <ul className="flex items-center gap-x-4">
-                                    {
-                                        LANGUAGES.map((language, index) => (
+                        {/* Large Screen View */}
+                        <div className="hidden lg:flex items-center justify-between w-full">
+                            <div className="flex items-center gap-x-4">
+                                {/* Logo */}
+                                <div>
+                                    <Image
+                                        src='/img/logo.png'
+                                        alt='Logo'
+                                        width={100}
+                                        height={100}
+                                        onClick={() => router.push('/')}
+                                        className="cursor-pointer"
+                                    />
+                                </div>
+                                {/* Language Options */}
+                                <div>
+                                    <ul className="flex items-center gap-x-4">
+                                        {LANGUAGES.map((language, index) => (
                                             <li
                                                 key={index}
                                                 onClick={() => setSelectedlanguage(language)}
@@ -74,54 +79,116 @@ const Navbar = () => {
                                             >
                                                 {language}
                                             </li>
-                                        ))
-                                    }
-                                </ul>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                            {/* Right div */}
+                            <div className="flex items-center gap-x-5">
+                                {/* Youtube Link */}
+                                <div
+                                    className="group flex items-center gap-x-2 transition-all duration-300 ease-in-out hover:text-gray-900 cursor-pointer"
+                                    onClick={handleYoutubeClick}
+                                >
+                                    <Image
+                                        src='/img/logo.png'
+                                        alt='Logo'
+                                        width={60}
+                                        height={60}
+                                        className="transition-all duration-300 ease-in-out grayscale group-hover:grayscale-0 relative -bottom-1.5"
+                                    />
+                                    <span className="text-gray-500 text-xl group-hover:text-black transition-all duration-300 ease-in-out">
+                                        on
+                                    </span>
+                                    <Image
+                                        src='/img/youtubeLogo.png'
+                                        alt='Logo'
+                                        width={60}
+                                        height={60}
+                                        className="transition-all duration-300 ease-in-out group-hover:grayscale-0 grayscale"
+                                    />
+                                </div>
+                                {/* Search Box */}
+                                <div className="flex items-center border rounded-md px-2">
+                                    <Search className="text-gray-900" />
+                                    <Input
+                                        className="border-none focus-visible:ring-0 w-48"
+                                        placeholder="Search news"
+                                        onChange={handleInputChange}
+                                        onKeyDown={handleKeyDown}
+                                    />
+                                </div>
                             </div>
                         </div>
-                        {/* right div */}
-                        <div className="flex items-center gap-x-5">
-                            {/* Youtube Link */}
-                            <div
-                                className="group flex items-center gap-x-2 transition-all duration-300 ease-in-out hover:text-gray-900 cursor-pointer"
-                                onClick={() => {
-                                    window.open("https://www.youtube.com/channel/UCt0n9skCrGvC97AIywt6o7A", "_blank", "noopener,noreferrer");
-                                }}
-                            >
+
+                        {/* Medium & Small Screen View */}
+                        <div className="flex lg:hidden items-center justify-between w-full p-4">
+                            <div className="flex items-center gap-x-4">
                                 <Image
                                     src='/img/logo.png'
                                     alt='Logo'
-                                    width={60}
-                                    height={60}
-                                    className="transition-all duration-300 ease-in-out grayscale group-hover:grayscale-0 relative -bottom-1.5"
+                                    width={80}
+                                    height={80}
+                                    onClick={() => router.push('/')}
+                                    className="cursor-pointer"
                                 />
-                                <span className="text-gray-500 text-xl group-hover:text-black transition-all duration-300 ease-in-out">
-                                    on
-                                </span>
-                                <Image
-                                    src='/img/youtubeLogo.png'
-                                    alt='Logo'
-                                    width={60}
-                                    height={60}
-                                    className="transition-all duration-300 ease-in-out group-hover:grayscale-0 grayscale"
-                                />
+                                <div className="flex items-center">
+                                    <Youtube
+                                        onClick={handleYoutubeClick}
+                                        className="text-gray-500 hover:text-red-600 transition-colors duration-300 mr-2 cursor-pointer"
+                                        size={30}
+                                    />
+                                    <div className="flex items-center border rounded-md px-1">
+                                        <Search className="text-gray-900" size={20} />
+                                        <Input
+                                            className="border-none focus-visible:ring-0 w-32 text-sm"
+                                            placeholder="Search"
+                                            onChange={handleInputChange}
+                                            onKeyDown={handleKeyDown}
+                                        />
+                                    </div>
+                                </div>
                             </div>
-                            {/* Search Box */}
-                            <div className="flex items-center border px-2">
-                                <Search className="text-gray-900" />
-                                <Input
-                                    className="border-none focus-visible:ring-0"
-                                    placeholder="Seach news"
-                                    onChange={handleInputChange}
-                                    onKeyDown={handleKeyDown}
-                                />
-                            </div>
+                            <DropdownMenu open={mobileMenuOpen} onOpenChange={setMobileMenuOpen}>
+                                <DropdownMenuTrigger>
+                                    <Menu className="h-6 w-6" />
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                    {/* Language Selection */}
+                                    {LANGUAGES.map((language, index) => (
+                                        <DropdownMenuItem
+                                            key={index}
+                                            onSelect={() => setSelectedlanguage(language)}
+                                            className={cn(selectedLanguage === language ? 'text-red-600 font-bold' : 'text-gray-600')}
+                                        >
+                                            {language}
+                                        </DropdownMenuItem>
+                                    ))}
+
+                                    {/* Menu Items */}
+                                    {[...MENUITEMS, ...OTHERITEMS].map((item, index) => (
+                                        <DropdownMenuItem
+                                            key={index}
+                                            onSelect={() => router.push(item.path)}
+                                            className={cn(
+                                                "cursor-pointer",
+                                                pathName === item.path
+                                                    ? "font-semibold text-[#9C1818]"
+                                                    : "text-gray-700"
+                                            )}
+                                        >
+                                            {item.name}
+                                        </DropdownMenuItem>
+                                    ))}
+                                </DropdownMenuContent>
+                            </DropdownMenu>
                         </div>
                     </div>
                 </MaxWidthWrapper>
             </div>
-            {/* Other Bar */}
-            <div className="bg-[#F2EEE3]">
+
+            {/* Bottom Bar - Large Screens */}
+            <div className="bg-[#F2EEE3] hidden lg:block">
                 <MaxWidthWrapper>
                     <div className="flex justify-between items-center px-4 pt-2">
                         {/* Left List */}
@@ -165,7 +232,7 @@ const Navbar = () => {
                     </div>
                 </MaxWidthWrapper>
             </div>
-            <div className="w-full h-1 bg-gray-300 -mt-0.5" />
+            <div className="w-full h-1 bg-gray-300 -mt-0.5 hidden lg:block" />
         </nav>
     )
 }
