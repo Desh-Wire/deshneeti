0e271faabc461a86be0d0b7b98cd92e366a2e173
PARMESH YADAV <parmesh20319@iiitd.ac.in>
2025-01-13 15:44:51 +0530

    some changes for production

--- src/components/AuthorItemHomePage.tsx
+++ src/components/AuthorItemHomePage.tsx
@@ -9,9 +9,9 @@ type JustInItemProps = {
 }
 
 const AuthorItem = ({ author, title, router, image, uuid }: JustInItemProps) => {
-    const handleAuthorClick = (author: string) => {
-        router.push(`/author/${author}`)
-    }
+    // const handleAuthorClick = (author: string) => {
+    //     router.push(`/author/${author}`)
+    // }
 
     const handleNewsClick = (uuid: string) => {
         router.push(`/news/${uuid}`)
@@ -23,10 +23,10 @@ const AuthorItem = ({ author, title, router, image, uuid }: JustInItemProps) => 
                 src={image}
                 alt={title}
                 className='object-cover cursor-pointer hover:scale-105 transition-transform rounded-full  p-4 aspect-square h-20'
-                onClick={() => handleAuthorClick(author.toLowerCase())}
+                onClick={() => {}}
             />
             <div className="flex flex-col gap-y-2">
-                <p className='font-bold uppercase tracking-wider text-red-700 cursor-pointer hover:text-black' onClick={() => handleAuthorClick(author.toLowerCase())}>
+                <p className='font-bold uppercase tracking-wider text-red-700 cursor-pointer hover:text-black' onClick={() => {}}>
                     {author}
                 </p>
                 <p className='font-semibold cursor-pointer hover:text-red-700' onClick={() => handleNewsClick(uuid)}>
